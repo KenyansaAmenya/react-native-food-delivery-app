446dbc7d2e1621a89bf12fa412cdfd1355e1b57d
Felix Amenya <kenyansafelix@gmail.com>
2024-05-22 13:15:19 +0300

    initial

--- app/screens/Home.jsx
+++ app/screens/Home.jsx
@@ -1,17 +1,48 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useContext, useState } from "react";
 import { COLORS, SIZES } from "../constants/theme";
 import { SafeAreaView } from "react-native-safe-area-context";
 import pages from './page.style'
 import uidata from "../constants/uidata";
+import HomeHeader from "../components/HomeHeader";
+import CategoryList from "../components/CategoryList";
+import ChoicesList from "../components/ChoicesList";
+import Heading from "../components/Heading";
+import NearByRestaurants from "../components/NearByRestaurants";
 
 const Home = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null)
+  const [selectedSection, setSelectedSection] = useState(null)
+  const [selectedValue, setSelectedValue] = useState(null)
+  const [selectedChoice, setSelectedChoice] = useState(null)
   
+ 
   return (
     <SafeAreaView>
       <View style={pages.viewOne}>
         <View style={pages.viewTwo}>
+          <HomeHeader />
 
+
+       <ScrollView 
+       showsVerticalScrollIndicator={false}
+       style={{borderBottomRadius: 30, borderBottomStartRadius: 30}}>
+
+        <CategoryList 
+        setSelectedCategory={setSelectedCategory}
+        setSelectedSection={setSelectedSection}
+        setSelectedValue={setSelectedValue}
+        />
+
+        <ChoicesList setSelectedChoice={setSelectedChoice} setSelectedSection={setSelectedSection}/>
+
+
+        <View>
+          <Heading heading={'Nearby Restaurants'} onPress={() => {}}/>
+
+          <NearByRestaurants />
+        </View>
+       </ScrollView>
         </View>
       </View>
     </SafeAreaView>
